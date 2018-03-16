9a9c7c4f4ebdb99c5f36e6cd7873efa790efe5a6
Ashon28 <37271346+Ashon28@users.noreply.github.com>
2018-03-16 09:14:09 +0000

    Add files via upload

--- ControlOptions.java
+++ ControlOptions.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -13,18 +16,94 @@ public class ControlOptions extends JPanel {
         clear.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("hello");
+                drawingArea.clearing();
                 drawingArea.repaint();
             }
         });
 
         JButton undo = new JButton("Undo");
+        undo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                drawingArea.undoing();
+                drawingArea.repaint();
+            }
+        });
 
         JButton redo = new JButton("Redo");
+        redo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                drawingArea.redoing();
+                drawingArea.repaint();
+            }
+        });
+
+        JCheckBox eraser = new JCheckBox("Erase");
+        eraser.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        JCheckBox ToggleSectorLines = new JCheckBox("Toggle Sector Lines");
+        ToggleSectorLines.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                drawingArea.toggleSectorLines();
+                drawingArea.repaint();
+            }
+        });
+
+
+
+        String[] colours = {"Green", "Orange", "Red", "Blue"};
+        Color[] colors = {Color.green,Color.orange,Color.red,Color.blue};
+        JComboBox colourList = new JComboBox(colours);
+        colourList.setSelectedIndex(0);
+        colourList.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(colourList.getSelectedItem());
+                Color colorSelection = colors[colourList.getSelectedIndex()];
+                drawingArea.setPenColour(colorSelection);
+
+            }
+        });
+
+
+
+
+        SpinnerModel penSize = new SpinnerNumberModel(2,1,10,1);
+        JSpinner penSpinner = new JSpinner(penSize);
+        penSpinner.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                System.out.println(penSpinner.getValue());
+                drawingArea.setPenSize((Integer) penSpinner.getValue());
+            }
+        });
+
+        SpinnerModel numberOfSectors = new SpinnerNumberModel(12,2,24,1);
+        JSpinner sectorSpinner = new JSpinner(numberOfSectors);
+        sectorSpinner.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                drawingArea.setNumberOfSectors((Integer) sectorSpinner.getValue());
+                drawingArea.repaint();
+            }
+        });
+
 
         add(clear);
         add(undo);
         add(redo);
+        add(eraser);
+        add(ToggleSectorLines);
+        add(colourList);
+        add(penSpinner);
+        add(sectorSpinner);
 
     }
 
--- DrawingArea.java
+++ DrawingArea.java
@@ -8,17 +8,111 @@ import java.util.ArrayList;
 
 public class DrawingArea extends JPanel implements MouseListener,MouseMotionListener {
 
-    int numberOfSecotrs = 12;
+    protected int numberOfSectors = 12;
     protected int x1 ;
     protected int y1 ;
     protected int cx,cy;
     protected int count = 0;
 
+    ArrayList<Path2D.Double> undoStorage = new ArrayList<Path2D.Double>();
+
     ArrayList<Path2D.Double> pathStorage = new ArrayList<Path2D.Double>();
+
+    ArrayList<Integer> penSizeStorage = new ArrayList<Integer>();
+
+    ArrayList<Integer> undoPenSizeStorage = new ArrayList<Integer>();
+
+    ArrayList<Color> colorStorage = new ArrayList<Color>();
+
+    ArrayList<Color> undoColorStorage = new ArrayList<Color>();
 
     Path2D.Double path1 = new Path2D.Double();
 
+    boolean start = true;
+
+    boolean currentlyClearing = false;
+
+    boolean currentlyUndoing = false;
+
+    boolean showSectorLines = true;
+
+    protected int penSize = 2;
+
+    Color penColour = Color.green;
+
+    public void setNumberOfSectors(int sectorNumber) {
+        numberOfSectors = sectorNumber;
+    }
+
+    public int getNumberOfSectors(){
+        return numberOfSectors;
+    }
+
+    public void toggleSectorLines(){
+        showSectorLines = !showSectorLines;
+    }
+
+    public void setPenSize(int pen){
+        penSize = pen;
+    }
+
+    public int getPenSize(){
+        return penSize;
+    }
+
+    public void setPenColour(Color colorChange){
+        penColour = colorChange;
+    }
+
+    public Color getPenColour() {
+        return penColour;
+    }
+
+    public void undoing(){
+        if (pathStorage.size() > 0) {
+            System.out.println("Undoing");
+            Path2D.Double recentPath = pathStorage.get(pathStorage.size() - 1);
+            undoStorage.add(recentPath);
+            undoColorStorage.add(colorStorage.get(colorStorage.size()-1));
+            undoPenSizeStorage.add(penSizeStorage.get(penSizeStorage.size()-1));
+
+            pathStorage.remove(pathStorage.size() - 1);
+            colorStorage.remove(colorStorage.size()-1);
+            penSizeStorage.remove(penSizeStorage.size()-1);
+            currentlyUndoing = true;
+        }
+    }
+
+    public void redoing(){
+        if(undoStorage.size() > 0){
+            System.out.println("Redoing");
+            Path2D.Double PathToRedo = undoStorage.get(undoStorage.size() -1);
+            pathStorage.add(PathToRedo);
+            colorStorage.add(undoColorStorage.get(undoColorStorage.size()-1));
+            penSizeStorage.add(undoPenSizeStorage.get(undoColorStorage.size()-1));
+
+            undoStorage.remove(undoStorage.size() - 1 );
+            undoColorStorage.remove(undoColorStorage.size() - 1);
+            undoPenSizeStorage.remove(undoPenSizeStorage.size() - 1 );
+        }
+    }
+
     public void clearing(){
+        System.out.println("Clearing");
+        pathStorage.clear();
+        colorStorage.clear();
+        penSizeStorage.clear();
+
+        undoStorage.clear();
+        undoColorStorage.clear();
+        undoPenSizeStorage.clear();
+
+        currentlyClearing = true;
+
+    }
+
+    public void erasing(){
+        System.out.println("Erasing");
 
     }
 
@@ -28,50 +122,95 @@ public class DrawingArea extends JPanel implements MouseListener,MouseMotionList
 
     public void paintComponent(Graphics g1){
 
+        //System.out.println("Painting");
+
         super.paintComponent(g1);
         Graphics2D g2 = (Graphics2D) g1;
 
-            for (int i = 0; i < numberOfSecotrs; i++) {
+        if (showSectorLines == true) {
+
+            for (int i = 0; i < numberOfSectors; i++) {
                 g2.setColor(Color.white);
                 g2.drawLine(450, 450, 450, 0);
-                g2.rotate(Math.toRadians(30), 450, 450);
-            }
-
-        g2.setPaint(Color.green);
-        g2.setStroke(new BasicStroke(2));
-
-        for(Path2D.Double path: pathStorage) {
-
-            path.moveTo(cx, cy);
-            path.quadTo(cx, cy, x1, y1);
-
-            for (int i = 0; i < 12; i++) {
-                g2.draw(path);
-                g2.rotate(Math.toRadians(30), 450, 450);
+                g2.rotate(Math.toRadians(360/numberOfSectors), 450, 450);
             }
 
         }
-        cx = x1;
-        cy = y1;
+
+        //System.out.println(start);
+
+        if (start == false && currentlyClearing == false) {
+
+            //System.out.println("Drawing");
+
+            g2.setPaint(penColour);
+            g2.setStroke(new BasicStroke(penSize));
+
+
+            //Path2D.Double currentpath = pathStorage.get(pathStorage.size()-1);
+
+            /*
+            if (currentlyUndoing == false) {
+                currentpath.moveTo(cx, cy);
+                currentpath.quadTo(cx, cy, x1, y1);
+            }
+            */
+
+            int pathNumber = 0;
+
+            for (Path2D.Double path : pathStorage) {
+
+                g2.setPaint(colorStorage.get(pathNumber));
+                g2.setStroke(new BasicStroke(penSizeStorage.get(pathNumber)));
+
+                for (int i = 0; i < numberOfSectors; i++) {
+                    g2.draw(path);
+                    g2.rotate(Math.toRadians(360/numberOfSectors), 450, 450);
+                }
+
+                pathNumber++;
+            }
+            /*
+            cx = x1;
+            cy = y1;
+            */
+
+        }
+
+        currentlyUndoing = false;
+        currentlyClearing = false;
 
     }
 
     public DrawingArea(){
         super();
         this.setBackground(Color.black);
-        pathStorage.add(path1);
+
+        //pathStorage.add(path1);
         this.addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent e) {
+
+                //start = false;
                 cx = e.getX();
                 cy = e.getY();
+                //pathStorage.add(new Path2D.Double());
+
+                //System.out.println("Clicked");
+
                 //repaint();
             }
 
             @Override
             public void mousePressed(MouseEvent e) {
+
+                //System.out.println("Pressed down");
+                start = false;
                 cx = e.getX();
                 cy = e.getY();
+                pathStorage.add(new Path2D.Double());
+                colorStorage.add(penColour);
+                penSizeStorage.add(penSize);
             }
 
             @Override
@@ -92,9 +231,19 @@ public class DrawingArea extends JPanel implements MouseListener,MouseMotionList
         this.addMouseMotionListener(new MouseMotionListener() {
             @Override
             public void mouseDragged(MouseEvent e) {
+                //System.out.println("Dragged");
                 x1 = e.getX();
                 y1 = e.getY();
+                start = false;
+
+                Path2D.Double currentpath = pathStorage.get(pathStorage.size()-1);
+                currentpath.moveTo(cx, cy);
+                currentpath.quadTo(cx, cy, x1, y1);
+
                 repaint();
+
+                cx = x1;
+                cy = y1;
             }
 
             @Override
